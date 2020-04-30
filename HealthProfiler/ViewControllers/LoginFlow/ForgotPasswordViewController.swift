e40fec54abb6cc53c97b4070d52d0d9fcb453084
Pooja Pandey <pooja_pande33@optum.com>
2020-04-30 12:52:24 +0530

    forgot password added

---
+++ HealthProfiler/ViewControllers/LoginFlow/ForgotPasswordViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ForgotPasswordViewController.swift
+//  HealthProfiler
+//
+//  Created by Pandey, Pooja on 29/04/20.
+//  Copyright © 2020 Pandey, Pooja. All rights reserved.
+//
+
+import UIKit
+
+class ForgotPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
