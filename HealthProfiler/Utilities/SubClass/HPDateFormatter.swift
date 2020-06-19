51660917d5252de44aa48f7d3a1965e2bd547b0a
Krishna Kant Kaira <krishna.kaira@simpplr.com>
2020-06-19 13:12:28 +0530

    Date extension replaced with new HPDateFormatter.

---
+++ HealthProfiler/Utilities/SubClass/HPDateFormatter.swift
@@ -0,0 +1,9 @@
+//
+//  HPDateFormatter.swift
+//  HealthProfiler
+//
+//  Created by Krishna Kant Kaira on 19/06/20.
+//  Copyright © 2020 Pandey, Pooja. All rights reserved.
+//
+
+import Foundation
